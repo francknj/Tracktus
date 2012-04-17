aff8d29251121ca460b25a15c33b8dccda033437
Pierre GOUDJO <pierre.goudjo@gmail.com>
2012-04-17 02:41:16 +0200

    [#27672021] Add another user data to fixtures
    I add an admin account and a supervisor

--- src/Tracktus/UserBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Tracktus/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -19,13 +19,35 @@ class LoadUserData implements FixtureInterface, ContainerAwareInterface
 
     public function load(ObjectManager $manager)
     {
+        //Get an userManager
+        $userManager = $this->container->get('fos_user.user_manager');
+        
+
+        //Create a simple member account
         $user = new User();
         $user->setUserName('ali')->setEmail('user@foo.com')
             ->setPlainPassword('71SECRET');
         $user->setEnabled(true);
-        $userManager = $this->container->get('fos_user.user_manager');
         $userManager->updatePassword($user);
         $manager->persist($user);
+
+        //Create an admin account
+        $projectLead = new User();
+        $projectLead->setUserName('fred')->setEmail('fred@foo.com')
+            ->setPlainPassword('71SECRET');
+        $projectLead->setEnabled(true);
+        $userManager->updatePassword($projectLead);
+        $manager->persist($projectLead);
+
+        //Create a supervisor
+        $supervisor = new User();
+        $supervisor->setUserName('yann')->setEmail('yann@foo.com')
+            ->setPlainPassword('71SECRET');
+        $supervisor->setEnabled(true);
+        $userManager->updatePassword($supervisor);
+        $manager->persist($supervisor);
+
+        //Save in database
         $manager->flush();
     }
 }
